c6bb73984c19d8ec1847c15c1120379f5cab33a3
Otávio <31575171+tavinhoo@users.noreply.github.com>
2024-02-12 13:17:23 -0300

    join query - projeção

--- 1_praticas/testes.sql
+++ 1_praticas/testes.sql
@@ -22,3 +22,42 @@ INSERT INTO CATEGORY VALUES(NULL, 'ELETRONICS', 1);
 INSERT INTO PRODUCT VALUES(NULL, 'Computer', 1100.0, 1);
 INSERT INTO PRODUCT VALUES(NULL, 'Hammer', 90.0, 2);
 INSERT INTO PRODUCT VALUES(NULL, 'Smart TV', 1700.0, 1);
+
++----+----------+---------+-------------+
+| ID | NAME     | PRICE   | CATEGORY_ID |
++----+----------+---------+-------------+
+|  1 | Computer | 1100.00 |           1 |
+|  2 | Hammer   |   90.00 |           2 |
+|  3 | Smart TV | 1700.00 |           1 |
++----+----------+---------+-------------+
++-------------+--------------+------+-----+---------+----------------+
+| Field       | Type         | Null | Key | Default | Extra          |
++-------------+--------------+------+-----+---------+----------------+
+| ID          | int          | NO   | PRI | NULL    | auto_increment |
+| NAME        | varchar(255) | NO   |     | NULL    |                |
+| PRICE       | float(10,2)  | NO   |     | NULL    |                |
+| CATEGORY_ID | int          | YES  | MUL | NULL    |                |
++-------------+--------------+------+-----+---------+----------------+
+
++----+------------+------+
+| ID | NAME       | TIER |
++----+------------+------+
+|  1 | TOOLS      |    2 |
+|  2 | COMPUTERS  |    1 |
+|  3 | ELETRONICS |    1 |
++----+------------+------+
+
++-------+--------------+------+-----+---------+----------------+
+| Field | Type         | Null | Key | Default | Extra          |
++-------+--------------+------+-----+---------+----------------+
+| ID    | int          | NO   | PRI | NULL    | auto_increment |
+| NAME  | varchar(255) | NO   |     | NULL    |                |
+| TIER  | int          | NO   |     | NULL    |                |
++-------+--------------+------+-----+---------+----------------+
+
+/* JUNÇÃO */ 
+SELECT P.ID, P.NAME, P.PRICE, C.ID AS CATEGORY_ID, C.NAME
+FROM PRODUCT P 
+INNER JOIN CATEGORY C 
+ON P.CATEGORY_ID = C.ID;
+
